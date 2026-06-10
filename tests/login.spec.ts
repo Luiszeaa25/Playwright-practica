5c2862e9e975897df9e82a2c48719be531a7bb41
Luiszeaa25 <luis.zea5000@gmail.com>
2026-06-10 00:56:50 -0600

    Se agregaron dos test más

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,30 @@
 import { test } from "@playwright/test";
 import { LoginPage } from "../page/LoginPage";
+import { fakerEN_US } from "@faker-js/faker";
 
 test ('Login test', async ({page}) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.gotoLoginPage();
-    await loginPage.login('Admin','admin123');
-    await loginPage.LoginSuccess();
+    await loginPage.validLogin('Admin','admin123');
+    await loginPage.loginSuccess();
 
 });
+
+test ('Login no exitoso', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.gotoLoginPage();
+    await loginPage.validLogin(fakerEN_US.person.firstName(), fakerEN_US.string.alphanumeric(6));
+    await loginPage.loginUnsuccesful();
+});
+
+test ('Hipervinculos',async ({page}) =>{
+    const loginPage = new LoginPage(page);
+    await loginPage.gotoLoginPage();
+    await loginPage.validateLinkedin();
+    await loginPage.validateFacebook();
+    await loginPage.validateTwitter();
+    await loginPage.validateYoutube();
+    await loginPage.validateOrangeHRM();
+
+} );
